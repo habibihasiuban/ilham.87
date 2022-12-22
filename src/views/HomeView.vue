772f3e29125844cc0e7add8edc1fe2c0e67d6f44
11950115079@students.uin-suska.ac.id <hasibuanilhamhabibi@gmail.com>
2022-12-23 01:44:52 +0700

    first commit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,76 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class ="container">
+<img src="index.jpg" alt="">
+    </div>
+    <section id="hero" class="d-flex align-items-center">
+        <div class="container position-relative">
+            <div class="row justify-content-center">
+                <div class="col-xl-7 col-lg-9 text-center mb-5">
+                    <h1><b>Selamat Datang di Halaman Utama</b></h1><br><h3>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Similique quod alias rem distinctio maiores, labore dolor perspiciatis architecto illum optio necessitatibus ipsam reprehenderit laborum nostrum iusto! Non molestias itaque inventore?</h3>
+                </div>
+            </div>
+            <div class="text-center">
+                <a href="#about" class="btn btn-danger mb-5">Lanjut</a>
+            </div>
+
+            <div class="row icon-boxes">
+                <div class="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0" data-aos="zoom-in"
+                    data-aos-delay="200">
+                    <div class="icon-box">
+                        <div class="icon">
+                            <i class="ri-stack-line"></i>
+                        </div>
+                        <h4>
+                            <a href="">CONTOH 1</a>
+                        </h4>
+                        <p class="description">
+                           Berikut adalah contoh web sederhana
+                        </p>
+                    </div>
+                </div>
+
+                <div class="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0" data-aos="zoom-in"
+                    data-aos-delay="300">
+                    <div class="icon-box">
+                        <div class="icon">
+                            <i class="ri-palette-line"></i>
+                        </div>
+                        <h4 class="title">
+                            <a href="">CONTOH 2</a>
+                        </h4>
+                        <p class="description">
+                            Berikut adalah contoh web sederhana
+                        </p>
+                    </div>
+                </div>
+
+                <div class="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0" data-aos="zoom-in"
+                    data-aos-delay="400">
+                    <div class="icon-box">
+                        <div class="icon">
+                            <i class="ri-command-line"></i>
+                        </div>
+                        <h4 class="title"><a href="">CONTOH 3</a></h4>
+                        <p class="description">
+                            Berikut adalah contoh web sederhana
+                        </p>
+                    </div>
+                </div>
+
+                <div class="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0" data-aos="zoom-in"
+                    data-aos-delay="500">
+                    <div class="icon-box">
+                        <div class="icon">
+                            <i class="ri-fingerprint-line"></i>
+                        </div>
+                        <h4 class="title"><a href="">CONTOH 4</a></h4>
+                        <p class="description">
+                            Berikut adalah contoh web sederhana
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- End Hero -->
 </template>
-
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
-</script>
