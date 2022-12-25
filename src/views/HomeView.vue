e962ae689cbf9f543e61d3c936044eed814858c0
ILHAM HABIBI HASIBUAN <81701971+habibihasiuban@users.noreply.github.com>
2022-12-25 15:18:41 +0700

    Update HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,7 +6,7 @@
         <div class="container position-relative">
             <div class="row justify-content-center">
                 <div class="col-xl-7 col-lg-9 text-center mb-5">
-                    <h1><b>Selamat Datang di Halaman Utama</b></h1><br><h3>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Similique quod alias rem distinctio maiores, labore dolor perspiciatis architecto illum optio necessitatibus ipsam reprehenderit laborum nostrum iusto! Non molestias itaque inventore?</h3>
+                    <h1><b>Tugas UAS TWEB</b></h1><br><h3>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Similique quod alias rem distinctio maiores, labore dolor perspiciatis architecto illum optio necessitatibus ipsam reprehenderit laborum nostrum iusto! Non molestias itaque inventore?</h3>
                 </div>
             </div>
             <div class="text-center">
