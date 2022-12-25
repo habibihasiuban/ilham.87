bfb0882540e625a1e63ab361c3d4849057b393c3
ILHAM HABIBI HASIBUAN <81701971+habibihasiuban@users.noreply.github.com>
2022-12-25 15:32:54 +0700

    Update HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -12,65 +12,19 @@
             <div class="text-center">
                 <a href="#about" class="btn btn-danger mb-5">Lanjut</a>
             </div>
-
-            <div class="row icon-boxes">
-                <div class="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0" data-aos="zoom-in"
-                    data-aos-delay="200">
-                    <div class="icon-box">
-                        <div class="icon">
-                            <i class="ri-stack-line"></i>
-                        </div>
-                        <h4>
-                            <a href="">CONTOH 1</a>
-                        </h4>
-                        <p class="description">
-                           Berikut adalah contoh web sederhana
-                        </p>
-                    </div>
-                </div>
-
-                <div class="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0" data-aos="zoom-in"
-                    data-aos-delay="300">
-                    <div class="icon-box">
-                        <div class="icon">
-                            <i class="ri-palette-line"></i>
-                        </div>
-                        <h4 class="title">
-                            <a href="">CONTOH 2</a>
-                        </h4>
-                        <p class="description">
-                            Berikut adalah contoh web sederhana
-                        </p>
-                    </div>
-                </div>
-
-                <div class="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0" data-aos="zoom-in"
-                    data-aos-delay="400">
-                    <div class="icon-box">
-                        <div class="icon">
-                            <i class="ri-command-line"></i>
-                        </div>
-                        <h4 class="title"><a href="">CONTOH 3</a></h4>
-                        <p class="description">
-                            Berikut adalah contoh web sederhana
-                        </p>
-                    </div>
-                </div>
-
-                <div class="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0" data-aos="zoom-in"
-                    data-aos-delay="500">
-                    <div class="icon-box">
-                        <div class="icon">
-                            <i class="ri-fingerprint-line"></i>
-                        </div>
-                        <h4 class="title"><a href="">CONTOH 4</a></h4>
-                        <p class="description">
-                            Berikut adalah contoh web sederhana
-                        </p>
-                    </div>
-                </div>
-            </div>
-        </div>
+            <div class="container text-center">
+  <div class="row align-items-start">
+    <div class="col">
+      One of three columns
+    </div>
+    <div class="col">
+      One of three columns
+    </div>
+    <div class="col">
+      One of three columns
+    </div>
+  </div>
+</div>
     </section>
     <!-- End Hero -->
 </template>
